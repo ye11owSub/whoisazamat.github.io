e352a0d1af3dcdc102635d8b1b6ca94521ad4e2e
whoisazamat <182308@niuitmo.com>
2016-10-06 00:30:48 +0300

    main section width fixed

---
+++ js/common.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+
+    // $('#event').click(function () {
+    //     $(#event).removeClass("event-active");
+    //     $(this).addClass("event-active");
+    // });
+
+    function makeActive(){
+       concole.log('1');
+    }
+});
