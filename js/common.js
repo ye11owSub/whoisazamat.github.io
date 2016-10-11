9ac95b76e54e74896e9954359c91171404a10098
ye11owSub <casio00@mail.ru>
2016-10-11 03:20:52 +0300

    slider, footer, info
    Нужно ссылки расставить, в карусели добавить кнопки, выбрать какое видео
    нужно вставить, изменить шрифты. И понять о какпих уведомлениях ты
    говоришь. Так вроде всё

--- js/common.js
+++ js/common.js
@@ -1,11 +1,32 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
     // $('#event').click(function () {
     //     $(#event).removeClass("event-active");
     //     $(this).addClass("event-active");
     // });
 
-    function makeActive(){
-       concole.log('1');
+    function makeActive() {
+        concole.log('1');
     }
+
+    $('.owl-carousel').owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 500,
+        loop: true,
+        margin: 10
+        // mouseDrag: false
+        // nav: true,
+        // navText: [
+        //     "<i class='icon-chevron-left icon-white'><</i>",
+        //     "<i class='icon-chevron-right icon-white'>></i>"
+        // ]
+    });
+    // $(".next_button").click(function(){
+    //     $('.owl-carousel').trigger("owl-carousel.next");
+    // });
+    // $(".prev_button").click(function(){
+    //     owl.trigger("owl-carousel.prev");
+    // });
+
 });
