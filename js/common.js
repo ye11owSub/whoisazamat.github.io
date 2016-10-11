e5040c08398ee71452843f0cad4f27f07b4ffee0
ye11owSub <casio00@mail.ru>
2016-10-11 21:22:15 +0300

    navButton
    доделал слайдер

--- js/common.js
+++ js/common.js
@@ -14,19 +14,18 @@ $(document).ready(function () {
         autoplay: true,
         smartSpeed: 500,
         loop: true,
-        margin: 10
-        // mouseDrag: false
-        // nav: true,
-        // navText: [
-        //     "<i class='icon-chevron-left icon-white'><</i>",
-        //     "<i class='icon-chevron-right icon-white'>></i>"
-        // ]
+        margin: 10,
+        mouseDrag: false,
+        nav: true,
+        navText: [
+            "<i class='icon-chevron-left icon-white'><</i>",
+            "<i class='icon-chevron-right icon-white'>></i>"
+        ]
     });
-    // $(".next_button").click(function(){
-    //     $('.owl-carousel').trigger("owl-carousel.next");
-    // });
-    // $(".prev_button").click(function(){
-    //     owl.trigger("owl-carousel.prev");
-    // });
+
+    $(".item").click(function() {
+        $(".item").removeClass("white");
+        $(this).toggleClass("white");
+    })
 
 });
