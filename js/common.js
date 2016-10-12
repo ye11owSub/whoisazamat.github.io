aa79b5de3b1d200d5f4b09c96e91d0836aa99497
ye11owSub <casio00@mail.ru>
2016-10-12 23:47:05 +0300

    arrorws

--- js/common.js
+++ js/common.js
@@ -11,17 +11,17 @@ $(document).ready(function () {
 
     $('.owl-carousel').owlCarousel({
         items: 3,
-        autoplay: true,
-        smartSpeed: 500,
         loop: true,
         margin: 10,
         mouseDrag: false,
         nav: true,
-        navText: [
-            "<i class='icon-chevron-left icon-white'><</i>",
-            "<i class='icon-chevron-right icon-white'>></i>"
-        ]
+        // navText: [
+        //     "<i class='icon-chevron-left'></i>",
+        //     "<i class='icon-chevron-right'></i>"
+        // ]
     });
+    $( ".owl-prev").html('<i class="fa fa-chevron-left"></i>');
+    $( ".owl-next").html('<i class="fa fa-chevron-right"></i>');
 
     $(".item").click(function() {
         $(".item").removeClass("white");
